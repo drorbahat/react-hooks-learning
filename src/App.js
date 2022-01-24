59529c191c092def4874d096c870069e543fc337
dror bahat <bahat.dror@gmail.com>
2022-01-24 13:10:04 +0200

    learning useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("")
+
+  const downCount = () => {
+    setCount(prevCount => prevCount - 1)
+    setColor("red")
+  }
+
+  const upCount = () => {
+    setCount(prevCount => prevCount + 1)
+    setColor("green")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={downCount}>-</button>
+      <span style={{ color: color }}>{count}</span>
+      <button onClick={upCount}>+</button>
     </div>
   );
 }
